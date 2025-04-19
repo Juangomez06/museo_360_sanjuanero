cbefe3e87772704ec45a4c039316b07b21c7c6db
Juan Pablo Gómez <juanpablogomez0023@gmail.com>
2025-04-19 17:12:35 -0500

    prueba 1, imagen sin carpeta

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ const viewer = pannellum.viewer("panorama", {
   scenes: {
     pano1: {
       title: "Entrada museo San Juanero",
-      panorama: "/assets/img/escenas/entrada.jpeg",
+      panorama: "entrada.jpeg",
       yaw: 0,
       pitch: 0,
       minHfov: 40,
