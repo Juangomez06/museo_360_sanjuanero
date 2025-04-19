d3ff52e6cce9f1bd4917277ee1f903ffb4fe7b2c
Juan Pablo Gómez <juanpablogomez0023@gmail.com>
2025-04-19 17:39:56 -0500

    asset 2

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const viewer = pannellum.viewer("panorama", {
   scenes: {
     pano1: {
       title: "Entrada museo San Juanero",
-      panorama: "{{ asset('assets/img/escenas/entrada.jpeg') }}",
+      panorama: "secure_asset('assets/img/escenas/entrada.jpeg') }}",
       yaw: 0,
       pitch: 0,
       minHfov: 40,
